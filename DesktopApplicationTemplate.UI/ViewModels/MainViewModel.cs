1dc018f453da6b86072974033f55d2f52d41d1b1
Matt Armstrong <75219976+Armstrm6@users.noreply.github.com>
2025-07-21 12:44:02 -0400

    Add RelayCommand and fix solution

--- DesktopApplicationTemplate.UI/ViewModels/MainViewModel.cs
+++ DesktopApplicationTemplate.UI/ViewModels/MainViewModel.cs
@@ -7,6 +7,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using DesktopApplicationTemplate.UI.Commands;
 
 namespace DesktopApplicationTemplate.UI.ViewModels
 {
