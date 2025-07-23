a0a9c7ad2ad20556c529249a4ab85710556b575e
Matt Armstrong <75219976+Armstrm6@users.noreply.github.com>
2025-07-23 15:58:22 -0400

    Fix FtpClient initialization

--- DesktopApplicationTemplate.UI/Services/FtpService.cs
+++ DesktopApplicationTemplate.UI/Services/FtpService.cs
@@ -8,7 +8,11 @@ namespace DesktopApplicationTemplate.UI.Services
         private readonly FtpClient _client;
         public FtpService(string host, int port, string user, string pass)
         {
-            _client = new FtpClient(host, port, user, pass);
+            var credentials = new System.Net.NetworkCredential(user, pass);
+            _client = new FtpClient(host, credentials)
+            {
+                Port = port
+            };
         }
 
         public async Task UploadAsync(string localPath, string remotePath)
