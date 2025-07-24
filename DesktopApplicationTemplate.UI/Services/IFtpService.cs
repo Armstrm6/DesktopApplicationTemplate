2d38a52af692e9b572786f7cd72b0ce480b97983
Matt Armstrong <75219976+Armstrm6@users.noreply.github.com>
2025-07-24 15:46:17 -0400

    Fix unit test compile errors

--- DesktopApplicationTemplate.UI/Services/IFtpService.cs
+++ DesktopApplicationTemplate.UI/Services/IFtpService.cs
@@ -4,6 +4,6 @@ namespace DesktopApplicationTemplate.UI.Services
 {
     public interface IFtpService
     {
-        Task UploadAsync(string localPath, string remotePath);
+        Task UploadAsync(string localPath, string remotePath, System.Threading.CancellationToken token = default);
     }
 }
