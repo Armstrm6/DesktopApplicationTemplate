93fc1553f2babaf5384782a96f59cd03ecd36826
Matt Armstrong <75219976+Armstrm6@users.noreply.github.com>
2025-08-28 12:45:12 -0400

    docs: record wpf workload limitation

--- DesktopApplicationTemplate.UI.Tests/TextBoxHintBehaviorTests.cs
+++ DesktopApplicationTemplate.UI.Tests/TextBoxHintBehaviorTests.cs
@@ -3,9 +3,11 @@ using DesktopApplicationTemplate.UI.Behaviors;
 using FluentAssertions;
 using Xunit;
 
+namespace DesktopApplicationTemplate.Tests;
+
 public class TextBoxHintBehaviorTests
 {
-    [Theory]
+    [WindowsTheory]
     [InlineData("ServiceName", "Service Name")]
     [InlineData("TcpIp", "Tcp Ip")]
     public void GetFriendlyName_Splits_CamelCase(string input, string expected)
@@ -13,7 +15,7 @@ public class TextBoxHintBehaviorTests
         TextBoxHintBehavior.GetFriendlyName(input).Should().Be(expected);
     }
 
-    [Fact]
+    [WindowsFact]
     public void GetFriendlyName_Throws_When_Null()
     {
         Action act = () => TextBoxHintBehavior.GetFriendlyName(null!);
