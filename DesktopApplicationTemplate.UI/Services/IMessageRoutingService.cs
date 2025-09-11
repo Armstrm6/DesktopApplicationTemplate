421053e951a011763cb5f5f80db4f449b4228a18
Matt Armstrong <75219976+Armstrm6@users.noreply.github.com>
2025-09-11 10:42:26 -0400

    Use ServiceType in message routing

--- DesktopApplicationTemplate.UI/Services/IMessageRoutingService.cs
+++ DesktopApplicationTemplate.UI/Services/IMessageRoutingService.cs
@@ -1,3 +1,5 @@
+using DesktopApplicationTemplate.Models;
+
 namespace DesktopApplicationTemplate.UI.Services;
 
 /// <summary>
@@ -8,20 +10,22 @@ public interface IMessageRoutingService
     /// <summary>
     /// Updates the latest message for the specified service.
     /// </summary>
+    /// <param name="serviceType">The category of the service.</param>
     /// <param name="serviceName">The unique name of the service.</param>
     /// <param name="message">The message to store.</param>
-    void UpdateMessage(string serviceName, string message);
+    void UpdateMessage(ServiceType serviceType, string serviceName, string message);
 
     /// <summary>
     /// Attempts to retrieve the latest message for the specified service.
     /// </summary>
+    /// <param name="serviceType">The category of the service.</param>
     /// <param name="serviceName">The unique name of the service.</param>
     /// <param name="message">The message, if found.</param>
     /// <returns><c>true</c> if a message exists for the service; otherwise, <c>false</c>.</returns>
-    bool TryGetMessage(string serviceName, out string? message);
+    bool TryGetMessage(ServiceType serviceType, string serviceName, out string? message);
 
     /// <summary>
-    /// Resolves <c>{ServiceName.Message}</c> tokens within the provided template.
+    /// Resolves <c>{ServiceType.ServiceName.Message}</c> tokens within the provided template.
     /// </summary>
     /// <param name="template">The template containing message tokens.</param>
     /// <returns>The template with tokens replaced by their corresponding messages.</returns>
