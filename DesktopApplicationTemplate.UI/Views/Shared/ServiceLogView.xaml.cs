17c3de138fba5f372223931a63e4f9870613ad54
Matt Armstrong <75219976+Armstrm6@users.noreply.github.com>
2025-10-03 13:45:34 -0400

    Align shared view namespaces

--- DesktopApplicationTemplate.UI/Views/Shared/ServiceLogView.xaml.cs
+++ DesktopApplicationTemplate.UI/Views/Shared/ServiceLogView.xaml.cs
@@ -1,6 +1,6 @@
 using System.Windows.Controls;
 
-namespace DesktopApplicationTemplate.UI.Views
+namespace DesktopApplicationTemplate.UI.Views.Shared
 {
     /// <summary>
     /// Interaction logic for ServiceLogView.xaml
