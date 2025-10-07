d1a6db8e2c0c8d08e707e9ebc21fb1b7de703e04
Matt Armstrong <75219976+Armstrm6@users.noreply.github.com>
2025-10-07 09:44:55 -0400

    Fix missing option namespaces and core FTP dependency

--- DesktopApplicationTemplate.UI/App.xaml.cs
+++ DesktopApplicationTemplate.UI/App.xaml.cs
@@ -3,6 +3,7 @@ using DesktopApplicationTemplate.Core.Services.Protocols.FileObserver;
 using DesktopApplicationTemplate.Core.Services.Protocols.Ftp;
 using DesktopApplicationTemplate.Core.Services.Protocols.Heartbeat;
 using DesktopApplicationTemplate.Core.Services.Protocols.Http;
+using DesktopApplicationTemplate.Core.Services.Protocols.Hid;
 using DesktopApplicationTemplate.Core.Services.Protocols.Mqtt;
 using DesktopApplicationTemplate.Core.Services.Protocols.Scp;
 using DesktopApplicationTemplate.Core.Services.Protocols.Tcp;
