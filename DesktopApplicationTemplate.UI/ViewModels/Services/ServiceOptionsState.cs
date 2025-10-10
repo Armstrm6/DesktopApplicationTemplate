50fdb15c0ea6c422494ecc6cc03907251a4e11e0
Matt Armstrong <75219976+Armstrm6@users.noreply.github.com>
2025-10-10 15:41:43 -0400

    Add Models namespace for ServiceOptionsState

--- DesktopApplicationTemplate.UI/ViewModels/Services/ServiceOptionsState.cs
+++ DesktopApplicationTemplate.UI/ViewModels/Services/ServiceOptionsState.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using DesktopApplicationTemplate.Core.Services;
+using DesktopApplicationTemplate.Models;
 
 namespace DesktopApplicationTemplate.UI.ViewModels.Services
 {
